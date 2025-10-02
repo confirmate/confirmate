e4687f5a75d280a483166d52d6c732951f733853
Christian Banse <oxisto@aybaze.com>
2025-10-02 09:29:51 +0200

    Adding typescript auto-generation client

--- ui/src/routes/+page.svelte
+++ ui/src/routes/+page.svelte
@@ -1,2 +1,17 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+
+	let { data }: PageProps = $props();
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+TOES:
+
+{#each data.toes as toe}
+	<div>
+		<h2>{toe.name}</h2>
+		<p>{toe.description}</p>
+	</div>
+{/each}
