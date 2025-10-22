e991a7b5f9be3161f266e3e587733b6f95c08371
Nico Haas <nico.haas@aisec.fraunhofer.de>
2025-10-22 09:15:43 +0200

    Add license headers

---
+++ core/service/orchestrator/db.go
@@ -0,0 +1,5 @@
+// Copyright 2025 Fraunhofer AISEC:
+// This code is licensed under the terms of the Apache License, Version 2.0.
+// See the LICENSE file in this project for details.
+
+package orchestrator
--- core/service/orchestrator/service.go
+++ core/service/orchestrator/service.go
@@ -1,3 +1,7 @@
+// Copyright 2025 Fraunhofer AISEC:
+// This code is licensed under the terms of the Apache License, Version 2.0.
+// See the LICENSE file in this project for details.
+
 package orchestrator
 
 import (
