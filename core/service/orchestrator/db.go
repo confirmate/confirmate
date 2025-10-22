825706d67864b2af124f3178ac1200bde6b77bbf
Nico Haas <nico.haas@aisec.fraunhofer.de>
2025-10-22 10:16:33 +0200

    Add orchestrator db types

--- core/service/orchestrator/db.go
+++ core/service/orchestrator/db.go
@@ -3,3 +3,18 @@
 // See the LICENSE file in this project for details.
 
 package orchestrator
+
+import (
+	"confirmate.io/core/api/orchestrator"
+)
+
+// TODO(all): Decide if we want to keep these types in Orchestrator DB or separate DB for each service. See https://github.com/confirmate/confirmate/issues/74
+var types = []any{
+	&orchestrator.TargetOfEvaluation{},
+	&orchestrator.Certificate{},
+	&orchestrator.State{},
+	&orchestrator.Catalog{},
+	&orchestrator.Category{},
+	&orchestrator.Control{},
+	&orchestrator.AuditScope{},
+}
