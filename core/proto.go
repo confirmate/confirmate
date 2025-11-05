b61ecce223c55f82d715b5bddd311e3a55d895da
Angelika Schneider <aruppel@gmx.de>
2025-11-05 10:54:42 +0100

    Remove clouditor_header from port.go file

--- core/proto.go
+++ core/proto.go
@@ -17,5 +17,5 @@ package core
 
 //go:generate buf generate
 //go:generate buf generate --template buf.openapi.gen.yaml --path api/orchestrator -o openapi/orchestrator
-//go:generate buf generate --exclude-path="internal/ontology/clouditor_header.proto" --template buf.gotag.gen.yaml
+//go:generate buf generate  --template buf.gotag.gen.yaml
 // //go:generate buf generate --template buf.gen.ontology.yaml --path policies/security-metrics/ontology/1.0/ontology.proto -o api/ontology
