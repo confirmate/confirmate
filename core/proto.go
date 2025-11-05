321e3e79815416b993ac2cf784145f1656b34eb8
Christian Banse <christian.banse@aisec.fraunhofer.de>
2025-11-05 10:19:52 +0100

    Temporarily commented out sec policies ontology

--- core/proto.go
+++ core/proto.go
@@ -17,4 +17,4 @@ package core
 
 //go:generate buf generate
 //go:generate buf generate --template buf.openapi.gen.yaml --path api/orchestrator -o openapi/orchestrator
-//go:generate buf generate --template buf.gen.ontology.yaml --path policies/security-metrics/ontology/1.0/ontology.proto -o api/ontology
+// //go:generate buf generate --template buf.gen.ontology.yaml --path policies/security-metrics/ontology/1.0/ontology.proto -o api/ontology
