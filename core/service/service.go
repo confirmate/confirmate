9e410e603e3230f30bc7f80f01e2ee2ca09b6164
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2025-12-12 17:05:52 +0100

    Add service option

--- core/service/service.go
+++ core/service/service.go
@@ -14,4 +14,8 @@
 // This file is part of Confirmate Core.
 
 // Package service implements common service functionality.
+
 package service
+
+// Option is a functional option type to configure services.
+type Option[T any] func(T)
