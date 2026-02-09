abf45234bc1056a5f3861e05eec66d50aa280489
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2026-02-09 13:09:16 +0100

    Git remove unnecessary check

--- core/api/evaluation/request_extensions.go
+++ core/api/evaluation/request_extensions.go
@@ -3,9 +3,5 @@ package evaluation
 // GetTargetOfEvaluationId is a shortcut to implement TargetOfEvaluationRequest. It returns
 // the target of evaluation ID of the inner object.
 func (req *CreateEvaluationResultRequest) GetTargetOfEvaluationId() string {
-	if req.GetResult() == nil {
-		return ""
-	}
-
 	return req.GetResult().GetTargetOfEvaluationId()
 }
