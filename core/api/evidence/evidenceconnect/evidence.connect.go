d9ff0d8ef7346f92b8d88568fb02824da54a0ec1
Christian Banse <christian.banse@aisec.fraunhofer.de>
2026-03-10 17:29:28 +0100

    Trying to fix merge

--- core/api/evidence/evidenceconnect/evidence.connect.go
+++ core/api/evidence/evidenceconnect/evidence.connect.go
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-// Copyright 2016-2025 Fraunhofer AISEC
-=======
 // Copyright 2025-2026 Fraunhofer AISEC
->>>>>>> main
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
