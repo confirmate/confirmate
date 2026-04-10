19b216071721a20d98be342e2622f1b63a3ce3af
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2026-04-10 09:16:09 +0200

    Fix CreateCatalog() test

--- core/service/orchestrator/catalogs_test.go
+++ core/service/orchestrator/catalogs_test.go
@@ -59,7 +59,7 @@ func TestService_CreateCatalog(t *testing.T) {
 			},
 			want: func(t *testing.T, got *connect.Response[orchestrator.Catalog], args ...any) bool {
 				assert.NotNil(t, got.Msg)
-				return assert.Equal(t, orchestratortest.MockCatalog1.Id, got.Msg.Id)
+				return assert.Equal(t, orchestratortest.MockCatalog1, got.Msg)
 			},
 			wantErr: assert.NoError,
 		},
@@ -73,7 +73,8 @@ func TestService_CreateCatalog(t *testing.T) {
 			},
 			want: assert.Nil[*connect.Response[orchestrator.Catalog]],
 			wantErr: func(t *testing.T, err error, msgAndArgs ...any) bool {
-				return assert.IsConnectError(t, err, connect.CodeInvalidArgument)
+				return assert.IsConnectError(t, err, connect.CodeInvalidArgument) &&
+					assert.ErrorContains(t, err, "invalid request")
 			},
 		},
 		{
