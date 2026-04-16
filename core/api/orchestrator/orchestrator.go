2a532a57afcda3305a5c944de470c7523b540889
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2026-04-16 15:20:36 +0200

    Update commits for function IsRelevantFor()

--- core/api/orchestrator/orchestrator.go
+++ core/api/orchestrator/orchestrator.go
@@ -2,16 +2,14 @@ package orchestrator
 
 import "slices"
 
-// IsRelevantFor checks, whether this control is relevant for the given audit scope. For now this mainly
-// checks, whether the assurance level matches, if the Audit Scope has one. In the future, this could also include checks, if
-// the control is somehow out of scope.
+// IsRelevantFor checks if the control is relevant for the given audit scope and catalog. This is determined by comparing the assurance levels of the control and the audit scope against the assurance levels defined in the catalog. If the control's assurance level is less than or equal to the audit scope's assurance level, then the control is considered relevant. In the future, this could also include checks, if the control is somehow out of scope.
 func (c *Control) IsRelevantFor(auditScope *AuditScope, catalog *Catalog) bool {
 	// If the catalog does not have an assurance level, we are good to go
 	if len(catalog.AssuranceLevels) == 0 {
 		return true
 	}
 
-	// If the control does not explicitly specify an assurance level, we are also ok
+	// If the control or the audit scope does not have an assurance level, we are good to go
 	if c.AssuranceLevel == nil || auditScope.AssuranceLevel == nil {
 		return true
 	}
@@ -19,7 +17,7 @@ func (c *Control) IsRelevantFor(auditScope *AuditScope, catalog *Catalog) bool {
 	// Otherwise, we need to retrieve the possible assurance levels (in order) from the catalogs and compare the
 	// indices
 	idxControl := slices.Index(catalog.AssuranceLevels, *c.AssuranceLevel)
-	idxToe := slices.Index(catalog.AssuranceLevels, *auditScope.AssuranceLevel)
+	idxAuditScope := slices.Index(catalog.AssuranceLevels, *auditScope.AssuranceLevel)
 
-	return idxControl <= idxToe
+	return idxControl <= idxAuditScope
 }
