e812e3b8e49720d87dfde908c94e8b090a11090b
Leutrim Shala <leutrim.shala@aisec.fraunhofer.de>
2026-04-21 14:26:15 +0200

    Add mock toe

--- core/service/orchestrator/toe.go
+++ core/service/orchestrator/toe.go
@@ -19,11 +19,11 @@ import (
 	"context"
 	"fmt"
 
+	"buf.build/go/protovalidate"
 	"confirmate.io/core/api/assessment"
 	"confirmate.io/core/api/orchestrator"
 	"confirmate.io/core/service"
-
-	"buf.build/go/protovalidate"
+	"confirmate.io/core/service/evidence/evidencetest"
 	"connectrpc.com/connect"
 	"github.com/google/uuid"
 	"google.golang.org/protobuf/types/known/emptypb"
@@ -286,6 +286,43 @@ func (svc *Service) CreateDefaultTargetOfEvaluation() (target *orchestrator.Targ
 			Description: "This is the default target of evaluation",
 			CreatedAt:   now,
 			UpdatedAt:   now,
+			ConfiguredMetrics: []*assessment.Metric{
+				{
+					Id:          evidencetest.MockMetricID1,
+					Name:        evidencetest.MockMetricName1,
+					Category:    evidencetest.MockMetricCategory1,
+					Description: evidencetest.MockMetricDescription1,
+					Version:     evidencetest.MockMetricVersion1,
+					Comments:    evidencetest.MockMetricComments1,
+				},
+			},
+			TargetType: orchestrator.TargetOfEvaluation_TARGET_TYPE_CLOUD,
+		}
+
+		// Save it in the database
+		err = svc.db.Create(target)
+		if err != nil {
+			return nil, fmt.Errorf("storage error: %w", err)
+		}
+
+		// Create a default target of evaluation
+		target = &orchestrator.TargetOfEvaluation{
+			Id:          "22222222-2222-2222-2222-222222222222",
+			Name:        "Default Target of Evaluation2",
+			Description: "This is the default target of evaluation",
+			CreatedAt:   now,
+			UpdatedAt:   now,
+			ConfiguredMetrics: []*assessment.Metric{
+				{
+					Id:          evidencetest.MockMetricID1,
+					Name:        evidencetest.MockMetricName1,
+					Category:    evidencetest.MockMetricCategory1,
+					Description: evidencetest.MockMetricDescription1,
+					Version:     evidencetest.MockMetricVersion1,
+					Comments:    evidencetest.MockMetricComments1,
+				},
+			},
+			TargetType: orchestrator.TargetOfEvaluation_TARGET_TYPE_CLOUD,
 		}
 
 		// Save it in the database
