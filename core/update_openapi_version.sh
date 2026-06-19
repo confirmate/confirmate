6625b15f4ce922b4c925fdaee74b346a6784d685
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2026-06-19 09:44:38 +0200

    Only set titel in evidence file

--- core/update_openapi_version.sh
+++ core/update_openapi_version.sh
@@ -3,21 +3,22 @@
 VERSION=$(git describe --tags)
 
 for file in api/*/openapi.yaml; do
-   case "$file" in
-      *evidence/openapi.yaml)
-        TITLE="Evidence Store API"
-        ;;
-    esac
-
   if [[ -f "$file" ]]; then
     if [[ "$OSTYPE" == "darwin"* ]]; then
       sed -i '' "s|version: .*|version: $VERSION|" "$file"
-       [[ -n $TITLE ]] && sed -i '' "s|title: .*|title: $TITLE|" "$file"
+      if [[ "$file" == *"evidence/openapi.yaml" ]]; then
+        TITLE="Evidence Store API"
+        sed -i '' "s|title: .*|title: $TITLE|" "$file"
+        echo "Title of $file set to $TITLE."
+      fi
     else
       sed -i "s|version: .*|version: $VERSION|" "$file"
-      [[ -n $TITLE ]] && sed -i "s|title: .*|title: $TITLE|" "$file"
+      if [[ "$file" == *"evidence/openapi.yaml" ]]; then
+        TITLE="Evidence Store API"
+        sed -i "s|title: .*|title: $TITLE|" "$file"
+        echo "Title of $file set to $TITLE."
+      fi
     fi
     echo "Version of $file set to $VERSION."
-     [[ -n $TITLE ]] && echo "Title of $file set to $TITLE."
   fi
 done
