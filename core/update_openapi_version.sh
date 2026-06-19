98f0a8bb61d1154d39f5f8fbb32c0cf995a736a4
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2026-06-19 09:42:08 +0200

    clean up

--- core/update_openapi_version.sh
+++ core/update_openapi_version.sh
@@ -3,12 +3,21 @@
 VERSION=$(git describe --tags)
 
 for file in api/*/openapi.yaml; do
+   case "$file" in
+      *evidence/openapi.yaml)
+        TITLE="Evidence Store API"
+        ;;
+    esac
+
   if [[ -f "$file" ]]; then
     if [[ "$OSTYPE" == "darwin"* ]]; then
       sed -i '' "s|version: .*|version: $VERSION|" "$file"
+       [[ -n $TITLE ]] && sed -i '' "s|title: .*|title: $TITLE|" "$file"
     else
       sed -i "s|version: .*|version: $VERSION|" "$file"
+      [[ -n $TITLE ]] && sed -i "s|title: .*|title: $TITLE|" "$file"
     fi
     echo "Version of $file set to $VERSION."
+     [[ -n $TITLE ]] && echo "Title of $file set to $TITLE."
   fi
 done
