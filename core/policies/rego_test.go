8f5bc0c8035446c0818c776c5a8a062e9ca17be6
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2026-09-14 15:00:35 +0200

    Extend tests

--- core/policies/rego_test.go
+++ core/policies/rego_test.go
@@ -23,6 +23,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/open-policy-agent/opa/v1/rego"
 	"google.golang.org/protobuf/types/known/durationpb"
 	"google.golang.org/protobuf/types/known/structpb"
@@ -605,7 +606,7 @@ func Test_regoEval_evalMap(t *testing.T) {
 					Message: assessment.DefaultCompliantMessage,
 				}
 
-				return assert.Equal(t, want, got)
+				return assert.Equal(t, want, got, cmpopts.IgnoreFields(CombinedResult{}, "Message", "ComparisonResult")) && assert.NotEmpty(t, got.Message) && assert.NotEmpty(t, got.ComparisonResult)
 			},
 			wantErr: assert.NoError,
 		},
@@ -651,7 +652,7 @@ func Test_regoEval_evalMap(t *testing.T) {
 					Message: assessment.DefaultNonCompliantMessage,
 				}
 
-				return assert.Equal(t, want, got)
+				return assert.Equal(t, want, got, cmpopts.IgnoreFields(CombinedResult{}, "Message", "ComparisonResult")) && assert.NotEmpty(t, got.Message) && assert.NotEmpty(t, got.ComparisonResult)
 			},
 			wantErr: assert.NoError,
 		},
