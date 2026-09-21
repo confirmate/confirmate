ae2a4aad51b181d4341b5da28853bb5ed8077a58
Angelika Schneider <angelika.schneider@aisec.fraunhofer.de>
2026-09-21 16:24:09 +0200

    Add log

--- core/service/assessment/service.go
+++ core/service/assessment/service.go
@@ -576,6 +576,7 @@ func (svc *Service) MetricConfiguration(ctx context.Context, TargetOfEvaluationI
 
 		svc.confMutex.Lock()
 		// Update the metric configuration
+		slog.Info("Update cachedConfigurations", slog.String("metric id", config.MetricId))
 		svc.cachedConfigurations[key] = cache
 		defer svc.confMutex.Unlock()
 	}
